1ee7a8d8cde6ba2dd13d35e90e824e8e5ba7db76
Josh Cheek <josh.cheek@gmail.com>
2015-03-28 20:08:56 -0600

    Update threadsafe benchmarks

--- benchmarks/threadsafe_let_block.rb
+++ benchmarks/threadsafe_let_block.rb
@@ -5,7 +5,8 @@ require 'rspec/expectations'
 # benchmark requested in this PR         - https://github.com/rspec/rspec-core/pull/1858
 #
 # I ran these from lib root by adding "gem 'benchmark-ips'" to ../Gemfile-custom
-# then ran `bundle exec ruby benchmarks/threadsafe_let_block.rb`
+# then ran `bundle install --standalone --binstubs bundle/bin`
+# then ran `ruby --disable-gems -I lib -I "$PWD/bundle" -r bundler/setup -S benchmarks/threadsafe_let_block.rb`
 
 # The old, non-thread safe implementation, imported from the `master` branch and pared down.
 module OriginalNonThreadSafeMemoizedHelpers
@@ -61,19 +62,15 @@ class HostBase
   # wires the implementation
   # adds `let(:name) { nil }`
   # returns `Class.new(self) { let(:name) { super() } }`
-  def self.prepare_using(memoized_helpers)
+  def self.prepare_using(memoized_helpers, options={})
     include memoized_helpers
     extend memoized_helpers::ClassMethods
     memoized_helpers.define_helpers_on(self)
+
+    define_method(:initialize, &options[:initialize]) if options[:initialize]
     let(:name) { nil }
 
-    counter_class = Class.new(self) do
-      memoized_helpers.define_helpers_on(self)
-      counter = 0
-      let(:count) { counter += 1 }
-    end
-
-    verify_memoization_with(counter_class)
+    verify_memoizes memoized_helpers, options[:verify]
 
     Class.new(self) do
       memoized_helpers.define_helpers_on(self)
@@ -81,8 +78,14 @@ class HostBase
     end
   end
 
-  def self.verify_memoization_with(counter_class)
+  def self.verify_memoizes(memoized_helpers, additional_verification)
     # Since we're using custom code, ensure it actually memoizes as we expect...
+    counter_class = Class.new(self) do
+      include RSpec::Matchers
+      memoized_helpers.define_helpers_on(self)
+      counter = 0
+      let(:count) { counter += 1 }
+    end
     extend RSpec::Matchers
 
     instance_1 = counter_class.new
@@ -92,36 +95,26 @@ class HostBase
     instance_2 = counter_class.new
     expect(instance_2.count).to eq(2)
     expect(instance_2.count).to eq(2)
+
+    instance_3 = counter_class.new
+    instance_3.instance_eval &additional_verification if additional_verification
   end
-end
-
-ConfigNonThreadSafeMemoizedHelpers = ::RSpec::Core::MemoizedHelpers.dup
-ConfigNonThreadSafeMemoizedHelpers.module_eval do
-  def __memoized
-    @__memoized ||= NonThreadSafeMemoized.new
-  end
-
-  class NonThreadSafeMemoized
-    def initialize
-      @memoized = {}
-    end
-
-    def fetch_or_store(key)
-      @memoized.fetch(key) { @memoized[key] = yield }
-    end
-  end
-end
-
-class ThreadSafeHost < HostBase
-  Subclass = prepare_using RSpec::Core::MemoizedHelpers
 end
 
 class OriginalNonThreadSafeHost < HostBase
   Subclass = prepare_using OriginalNonThreadSafeMemoizedHelpers
 end
 
+class ThreadSafeHost < HostBase
+  Subclass = prepare_using RSpec::Core::MemoizedHelpers,
+    :initialize => lambda { |*| RSpec.configuration.threadsafe = true; super() },
+    :verify     => lambda { |*| expect(__memoized).to be_a_kind_of RSpec::Core::MemoizedHelpers::ThreadsafeMemoized }
+end
+
 class ConfigNonThreadSafeHost < HostBase
-  Subclass = prepare_using ConfigNonThreadSafeMemoizedHelpers
+  Subclass = prepare_using RSpec::Core::MemoizedHelpers,
+    :initialize => lambda { |*| RSpec.configuration.threadsafe = false; super() },
+    :verify     => lambda { |*| expect(__memoized).to be_a_kind_of RSpec::Core::MemoizedHelpers::NonThreadSafeMemoized }
 end
 
 def title(title)
@@ -211,12 +204,12 @@ __END__
 #  versions  #
 #            #
 ##############
-RUBY_VERSION             2.1.5
-RUBY_PLATFORM            x86_64-darwin12.0
+RUBY_VERSION             2.2.0
+RUBY_PLATFORM            x86_64-darwin13
 RUBY_ENGINE              ruby
-ruby -v                  ruby 2.1.5p273 (2014-11-13 revision 48405) [x86_64-darwin12.0]
+ruby -v                  ruby 2.2.0p0 (2014-12-25 revision 49005) [x86_64-darwin13]
 Benchmark::IPS::VERSION  2.1.1
-rspec-core SHA           e32ada9cd4423cdf06df9ef6a99f8ad7b6e13bec
+rspec-core SHA           048643ba3873c2f55d3bc5dc31334f080f6cbeaf
 
 ##########################################
 #                                        #
@@ -225,23 +218,23 @@ rspec-core SHA           e32ada9cd4423cdf06df9ef6a99f8ad7b6e13bec
 ##########################################
 Calculating -------------------------------------
 non-threadsafe (original)
-                        28.248k i/100ms
+                        54.820k i/100ms
 non-threadsafe (config)
-                        23.804k i/100ms
+                        32.292k i/100ms
 threadsafe
-                        13.001k i/100ms
+                        21.787k i/100ms
 -------------------------------------------------
 non-threadsafe (original)
-                        478.456k (±13.1%) i/s -      2.373M
+                        837.649k (± 6.8%) i/s -      4.221M
 non-threadsafe (config)
-                        465.080k (±17.6%) i/s -      2.261M
+                        422.334k (± 6.9%) i/s -      2.131M
 threadsafe
-                        240.298k (± 9.2%) i/s -      1.196M
+                        249.748k (± 5.0%) i/s -      1.264M
 
 Comparison:
-non-threadsafe (original):   478456.0 i/s
-non-threadsafe (config)  :   465080.3 i/s - 1.03x slower
-threadsafe               :   240298.3 i/s - 1.99x slower
+non-threadsafe (original):   837648.9 i/s
+non-threadsafe (config)  :   422334.3 i/s - 1.98x slower
+threadsafe               :   249747.7 i/s - 3.35x slower
 
 ###################################################
 #                                                 #
@@ -250,23 +243,23 @@ threadsafe               :   240298.3 i/s - 1.99x slower
 ###################################################
 Calculating -------------------------------------
 non-threadsafe (original)
-                        20.678k i/100ms
+                        28.190k i/100ms
 non-threadsafe (config)
-                        17.564k i/100ms
+                        21.302k i/100ms
 threadsafe
-                        12.120k i/100ms
+                        15.471k i/100ms
 -------------------------------------------------
 non-threadsafe (original)
-                        272.546k (± 8.0%) i/s -      1.365M
+                        342.177k (± 7.1%) i/s -      1.720M
 non-threadsafe (config)
-                        226.533k (± 7.1%) i/s -      1.142M
+                        242.997k (± 5.5%) i/s -      1.214M
 threadsafe
-                        146.561k (± 7.2%) i/s -    739.320k
+                        173.325k (± 6.1%) i/s -    866.376k
 
 Comparison:
-non-threadsafe (original):   272545.7 i/s
-non-threadsafe (config)  :   226532.8 i/s - 1.20x slower
-threadsafe               :   146561.4 i/s - 1.86x slower
+non-threadsafe (original):   342176.8 i/s
+non-threadsafe (config)  :   242997.4 i/s - 1.41x slower
+threadsafe               :   173325.4 i/s - 1.97x slower
 
 #######################################
 #                                     #
@@ -275,23 +268,23 @@ threadsafe               :   146561.4 i/s - 1.86x slower
 #######################################
 Calculating -------------------------------------
 non-threadsafe (original)
-                        30.780k i/100ms
+                        41.413k i/100ms
 non-threadsafe (config)
-                        26.364k i/100ms
+                        27.500k i/100ms
 threadsafe
-                        14.261k i/100ms
+                        17.971k i/100ms
 -------------------------------------------------
 non-threadsafe (original)
-                        478.578k (± 9.0%) i/s -      2.401M
+                        599.117k (± 5.8%) i/s -      3.023M
 non-threadsafe (config)
-                        395.975k (± 7.7%) i/s -      1.977M
+                        349.744k (± 5.9%) i/s -      1.760M
 threadsafe
-                        176.326k (± 6.5%) i/s -    884.182k
+                        201.316k (± 6.3%) i/s -      1.006M
 
 Comparison:
-non-threadsafe (original):   478578.3 i/s
-non-threadsafe (config)  :   395975.1 i/s - 1.21x slower
-threadsafe               :   176326.3 i/s - 2.71x slower
+non-threadsafe (original):   599117.3 i/s
+non-threadsafe (config)  :   349744.0 i/s - 1.71x slower
+threadsafe               :   201316.2 i/s - 2.98x slower
 
 #########################################
 #                                       #
@@ -300,20 +293,20 @@ threadsafe               :   176326.3 i/s - 2.71x slower
 #########################################
 Calculating -------------------------------------
 non-threadsafe (original)
-                        18.380k i/100ms
+                        23.086k i/100ms
 non-threadsafe (config)
-                        16.342k i/100ms
+                        18.249k i/100ms
 threadsafe
-                        10.262k i/100ms
+                        13.180k i/100ms
 -------------------------------------------------
 non-threadsafe (original)
-                        235.513k (± 6.9%) i/s -      1.176M
+                        290.688k (± 6.4%) i/s -      1.454M
 non-threadsafe (config)
-                        200.389k (± 5.4%) i/s -      1.013M
+                        214.459k (± 6.4%) i/s -      1.077M
 threadsafe
-                        121.804k (± 7.6%) i/s -    605.458k
+                        148.218k (± 4.9%) i/s -    751.260k
 
 Comparison:
-non-threadsafe (original):   235512.9 i/s
-non-threadsafe (config)  :   200389.0 i/s - 1.18x slower
-threadsafe               :   121803.9 i/s - 1.93x slower
+non-threadsafe (original):   290688.4 i/s
+non-threadsafe (config)  :   214459.2 i/s - 1.36x slower
+threadsafe               :   148218.3 i/s - 1.96x slower
